3ba42f9c8ec3ada353e4293a693aa5a1d86527ae
MiguelLorenzoLima <miguel.lorenzo.lb@gmail.com>
2025-11-21 15:41:31 -0300

    js adicionado

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,12 +4,31 @@ let container = document.querySelector('.container');
 let items = container.querySelectorAll('.list .item');
 let indicator = document.querySelector('.indicators');
 let dots = indicator.querySelectorAll('ul li');
+let number = document.querySelector('.number');
 
+let active = 0;
+let firstPosition = 0;
+let lastPosition = items.length - 1;
 
-nextButton.onclick = () =>{
-    
+function setSlider(){
+    let itemOld = container.querySelector('.list .item.active');    
+    itemOld.classList.remove('active');
+
+    let dotsOld = indicator.querySelector('ul li.active')
+    dotsOld.classList.remove('active');
+    dots[active].classList.add('active');
+
+    number.innerHTML = '0' + (active + 1);
 }
 
-prevButton.onclick = () =>{
-    
+nextButton.onclick = () => {
+    active = active + 1 > lastPosition ? 0 : active + 1;
+    setSlider();
+    items[active].classList.add('active');
+}
+
+prevButton.onclick = () => {
+    active = active - 1 < firstPosition ? lastPosition : active - 1;
+    setSlider();
+    items[active].classList.add('active');
 }
